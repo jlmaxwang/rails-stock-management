aba03b1c9b2f4e10f1f1719285f9c4813bf79026
Max Jilu Wang <wangjilu23@gmail.com>
2022-02-27 21:12:45 +1100

    work in progress:

--- app/models/powder.rb
+++ app/models/powder.rb
@@ -1,3 +1,4 @@
 class Powder < ApplicationRecord
   belongs_to :supplier
+  validates :name, :price_retail, :price_bulk, :location, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'powders#index'
+  resources :suppliers, only:[:index, :show, :new, :create, :edit, :update]
   resources :powders, except: :index
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
